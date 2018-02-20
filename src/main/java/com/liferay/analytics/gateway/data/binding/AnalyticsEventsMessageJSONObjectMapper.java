88c9030a52ab877efc54cfe7f4cd2851023119da
Inacio Nery <inacio.nery@liferay.com>
2018-02-20 17:23:19 -0300

    Update Message

--- src/main/java/com/liferay/analytics/gateway/data/binding/AnalyticsEventsMessageJSONObjectMapper.java
+++ src/main/java/com/liferay/analytics/gateway/data/binding/AnalyticsEventsMessageJSONObjectMapper.java
@@ -15,6 +15,8 @@
 package com.liferay.analytics.gateway.data.binding;
 
 import java.io.IOException;
+import java.text.FieldPosition;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +25,11 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.databind.util.ISO8601Utils;
 import com.liferay.analytics.gateway.model.AnalyticsEventsMessage;
+
 
 /**
  * @author Marcellus Tavares
@@ -40,9 +46,7 @@ public class AnalyticsEventsMessageJSONObjectMapper
 	}
 
 	@Override
-	public AnalyticsEventsMessage map(String jsonString)
-		throws IOException {
-
+	public AnalyticsEventsMessage map(String jsonString) throws IOException {
 		return _objectMapper.readValue(
 			jsonString, AnalyticsEventsMessage.class);
 	}
@@ -58,6 +62,11 @@ public class AnalyticsEventsMessageJSONObjectMapper
 
 		_objectMapper.configure(
 			DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		_objectMapper.configure(
+			SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+		_objectMapper.setDateFormat(new ISO8601MillisDateFormat());
 	}
 
 	private static final class AnalyticsEventsMessageMixIn {
@@ -84,11 +93,29 @@ public class AnalyticsEventsMessageJSONObjectMapper
 		@JsonProperty("applicationId")
 		private String _applicationId;
 
+		@JsonProperty("eventDate")
+		private Date _eventDate;
+
 		@JsonProperty("eventId")
 		private String _eventId;
 
 		@JsonProperty("properties")
 		private Map<String, String> _properties;
+
+	}
+
+	private final class ISO8601MillisDateFormat extends ISO8601DateFormat {
+
+		@Override
+		public StringBuffer format(
+			Date date, StringBuffer toAppendTo, FieldPosition fieldPosition) {
+
+			String value = ISO8601Utils.format(date, true);
+
+			toAppendTo.append(value);
+
+			return toAppendTo;
+		}
 
 	}
 
